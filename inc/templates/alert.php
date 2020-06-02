6b27f0fb4e59df401b06cff73995bda049e56535
Sean O'Connor <seanoconnor941@gmail.com>
2020-06-02 13:47:35 -0600

    toggle alert bar visibility

--- inc/templates/alert.php
+++ inc/templates/alert.php
@@ -1,5 +1,7 @@
-<div id="top-alert" class="alert">
-    <span id="top-alert__text" class="alert__text">
-        <?= get_alert_text() ?>
-    </span>
-</div>
+<?php if (true == get_theme_mod('alert_toggle')) : ?>
+    <div id="top-alert" class="alert">
+        <span id="top-alert__text" class="alert__text">
+            <?= get_alert_text() ?>
+        </span>
+    </div>
+<?php endif; ?>
