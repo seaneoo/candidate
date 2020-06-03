8e44f34de4f7a3fe20cdac96a4a67ef44bcaaa4c
Sean O'Connor <seanoconnor941@gmail.com>
2020-06-02 19:47:19 -0600

    placeholder hero for non-home pages

--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
-<div class="hero" style="background-image: url('<?= (has_post_thumbnail()) ? get_the_post_thumbnail_url() : '' ?>')"> </div>
+<div class="hero" style="background-image: url('<?= (has_post_thumbnail()) ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/hero-placeholder.png' ?>')"> </div>
 
 <!-- WordPress content -->
 <?php if (have_posts()) : while (have_posts()) : the_post() ?>
-                <?php the_content() ?>
+        <?php the_content() ?>
 <?php endwhile;
 endif; ?>
 
